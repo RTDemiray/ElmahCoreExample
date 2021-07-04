158217fa358a45d6be22cb0c8ec25339a93ee7ee
Recep Tayyip Demiray <43641933+RTDemiray@users.noreply.github.com>
2021-07-04 14:47:46 +0300

    Adding Elmah log library

---
+++ ElmahCoreExample/ElmahCustom/CmsErrorLogFilter.cs
@@ -0,0 +1,19 @@
+﻿using ElmahCore;
+using Microsoft.AspNetCore.Http;
+
+namespace ElmahCoreExample.ElmahCustom
+{
+    public class CmsErrorLogFilter : IErrorFilter
+    {
+        public void OnErrorModuleFiltering(object sender, ExceptionFilterEventArgs args)
+        {
+            if (args.Context is HttpContext httpContext)
+            {
+                if (httpContext.Response.StatusCode == StatusCodes.Status404NotFound) //Hata kodu 404 olanları filtrele ve dismiss et.
+                {
+                    args.Dismiss();
+                }   
+            }
+        }
+    }
+}
